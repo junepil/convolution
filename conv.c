96b3f08cff17c39aa692968a35dd30d778a1c53f
Junepil Lee <hanu9257@gmail.com>
2025-10-17 23:25:50 +0800

    Implement full parallelization with MPI

--- conv.c
+++ conv.c
@@ -83,44 +83,56 @@ Matrix read_matrix(char *path) {
   return mat;
 }
 
+// Replace your conv2d function with this new, corrected version
 Matrix conv2d(
-  Matrix input,
-  Matrix kernel,
-  int64_t stride_height,
-  int64_t stride_width)
-{
+    Matrix input,
+    Matrix kernel,
+    int64_t stride_height,
+    int64_t stride_width,
+    int64_t start_offset
+)
+{   
+  printf("start_offset: %lld\n", start_offset);
+
   int64_t diff_H = (int64_t)(kernel.H / 2);
   int64_t diff_W = (int64_t)(kernel.W / 2);
-
-  int64_t output_height = (input.H + stride_height - 1) / stride_height;
+  
+  int64_t num_strides_H = (input.H - start_offset + stride_height - 1) / stride_height;
+  int64_t output_height = num_strides_H;
   int64_t output_width = (input.W + stride_width - 1) / stride_width;
 
   Matrix output = create_matrix(output_height, output_width);
-
-  for (int64_t i = 0; i < input.H; i += stride_height) {
+  
+  // The main loop now starts from the corrected `first_i`.
+  for (int64_t i = start_offset; i < input.H; i += stride_height) {
     for (int64_t j = 0; j < input.W; j += stride_width) {
       float local_sum = 0.0;
-
-      // 경계 체크를 루프 밖으로 이동
+      
       const int64_t k_start = (i < diff_H) ? -i : -diff_H;
       const int64_t k_end = (i + diff_H >= input.H) ? input.H - 1 - i : diff_H;
       const int64_t l_start = (j < diff_W) ? -j : -diff_W;
       const int64_t l_end = (j + diff_W >= input.W) ? input.W - 1 - j : diff_W;
       
-      // 최적화된 내부 루프
       for (int64_t k = k_start; k <= k_end; ++k) {
-          const float *f_row = input.data[i + k];  // 포인터 캐싱
-          const float *g_row = kernel.data[k + diff_H];
-          
-          for (int64_t l = l_start; l <= l_end; ++l) {
-              local_sum += f_row[j + l] * g_row[l + diff_W];
+        const float *f_row = input.data[i + k];
+        
+        int64_t kernel_row = k + diff_H;
+        if (kernel_row < 0 || kernel_row >= kernel.H) {
+          continue;
+        }
+        const float *g_row = kernel.data[kernel_row];
+        
+        for (int64_t l = l_start; l <= l_end; ++l) {
+          int64_t kernel_col = l + diff_W;
+          if (kernel_col < 0 || kernel_col >= kernel.W) {
+            continue;
           }
+          local_sum += f_row[j + l] * g_row[kernel_col];
+        }
       }
-
-      output.data[i / stride_height][j / stride_width] = local_sum;
+      output.data[(i - start_offset) / stride_height][j / stride_width] = local_sum;
     }
   }
-
   return output;
 }
 
@@ -130,23 +142,52 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  if(rank == 0) { // root
+  if(rank != 0) { 
+    // Receive broadcasted kernel info
+    int64_t kernel_meta[4];
+    MPI_Bcast(kernel_meta, 4, MPI_INT64_T, 0, MPI_COMM_WORLD);
+    int64_t kernel_h = kernel_meta[KERNEL_H];
+    int64_t kernel_w = kernel_meta[KERNEL_W];
+    int64_t stride_h = kernel_meta[STRIDE_H];
+    int64_t stride_w = kernel_meta[STRIDE_W];
+    Matrix kernel = create_matrix(kernel_h, kernel_w);
+    MPI_Bcast(kernel.data[0], kernel_h * kernel_w, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    
+    // Receive metadata about the specific chunk for this worker
+    int64_t input_meta[4];
+    MPI_Recv(input_meta, 4, MPI_INT64_T, 0, META_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    int64_t input_h = input_meta[0];
+    int64_t input_w = input_meta[1];
+    int64_t send_start_row = input_meta[2];
+    int64_t expected_output_rows = input_meta[3];
+    
+    // Receive the padded input data
+    Matrix local_input = create_matrix(input_h, input_w);
+    MPI_Recv(local_input.data[0], input_h * input_w, MPI_FLOAT, 0, DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    int64_t border = send_start_row + (int64_t)(kernel.H / 2);
+    int64_t next_hop = ((int64_t)(border + stride_h - 1) / stride_h) * stride_h;
+    int64_t offset = next_hop - send_start_row;
+
+    Matrix local_output = conv2d(local_input, kernel, stride_h, stride_w, offset);
+    int64_t expected_elements = expected_output_rows * local_output.W;
+
+    MPI_Send(local_output.data[0], expected_elements, MPI_FLOAT, 0, DATA_TAG, MPI_COMM_WORLD);
+    free_matrix(local_input);
+    free_matrix(kernel);
+    free_matrix(local_output);
+  } 
+  else { 
     int opt, opt_idx;
     char *f_path = NULL, *g_path = NULL, *o_path = NULL;
     int64_t H = 0, W = 0, kH = 0 , kW = 0, sH = 1, sW = 1;
-  
     const struct option longopts[] = {
         {"kH", required_argument, NULL, OPT_kH},
         {"kW", required_argument, NULL, OPT_kW},
         {"sH", required_argument, NULL, OPT_sH},
         {"sW", required_argument, NULL, OPT_sW},
         {0, 0, 0, 0}};
-  
-    while (1)
-    {
-      opt = getopt_long_only(argc, argv, ":H:W:f:g:o:", longopts, &opt_idx);
-      if(opt == -1)
-        break;
+    while ((opt = getopt_long_only(argc, argv, ":H:W:f:g:o:", longopts, &opt_idx)) != -1) {
       switch (opt) {
       case 'H': H = atoll(optarg); break;
       case 'W': W = atoll(optarg); break;
@@ -160,130 +201,74 @@ int main(int argc, char** argv) {
       default: break;
       }
     }
-  
     Matrix input, kernel;
-    FILE *o_file = NULL;
-    
     if(H && W && kH && kW) {
       input = create_matrix(H, W);
       kernel = create_matrix(kH, kW);
-    }
-    else
-    {
-      if(f_path)
-        input = read_matrix(f_path);
-      if(g_path)
-        kernel = read_matrix(g_path);
+    } else {
+      if(f_path) input = read_matrix(f_path);
+      if(g_path) kernel = read_matrix(g_path);
     }
   
-    // parallelize here
-    // Matrix output = conv2d(input, kernel, sH, sW);
-    int64_t meta[4] = {kernel.H, kernel.W, sH, sW};
-    MPI_Bcast(meta, 4, MPI_INT64_T, 0, MPI_COMM_WORLD);
+    // Broadcast kernel info to all workers
+    int64_t kernel_meta[4] = {kernel.H, kernel.W, sH, sW};
+    MPI_Bcast(kernel_meta, 4, MPI_INT64_T, 0, MPI_COMM_WORLD);
     MPI_Bcast(kernel.data[0], kernel.H * kernel.W, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
+    // --- (Work distribution setup is unchanged) ---
     int64_t diff = kernel.H / 2;
     int64_t output_height = (input.H + sH - 1) / sH;
     int64_t output_width = (input.W + sW - 1) / sW;
     Matrix output = create_matrix(output_height, output_width);
-
     int64_t rows_per_proc = output.H / size;
     int64_t remainder = output.H % size;
 
+    // --- Distribute work and Gather results ---
     int64_t current_output_row = 0;
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i) {
       int64_t output_rows_for_this_proc = rows_per_proc + ((i < remainder) ? 1 : 0);
-      if(output_rows_for_this_proc == 0)
-        continue;
+      if(output_rows_for_this_proc == 0) continue;
 
       int64_t input_start_row = current_output_row * sH;
       int64_t input_end_row = (current_output_row + output_rows_for_this_proc - 1) * sH;
-
       int64_t send_start_row = (input_start_row - diff > 0) ? (input_start_row - diff) : 0;
       int64_t send_end_row = (input_end_row + diff) < input.H ? (input_end_row + diff) : input.H - 1;
-
-      int64_t send_row_count = send_end_row - send_start_row;
+      int64_t send_row_count = send_end_row - send_start_row + 1;
+      int64_t ghost_rows_top = input_start_row - send_start_row;
       
-      if(i == 0) {
+      if(i == 0) { // Root process does its own work
         Matrix local_input = create_matrix(send_row_count, input.W);
-        memcpy(local_input.data[0], input.data[0], send_row_count * input.W * sizeof(int64_t));
+        memcpy(local_input.data[0], input.data[send_start_row], send_row_count * input.W * sizeof(float));
 
-        Matrix local_output = conv2d(local_input, kernel, sH, sW);
+        Matrix local_output = conv2d(local_input, kernel, sH, sW, send_start_row);        
         memcpy(output.data[current_output_row], local_output.data[0], local_output.H * local_output.W * sizeof(float));
-      }
-      else
-      {
-        int64_t input_meta[2] = {send_row_count, input.W};
-        MPI_Send(input_meta, 2, MPI_INT64_T, i, META_TAG, MPI_COMM_WORLD);
+
+        free_matrix(local_input);
+        free_matrix(local_output);
+      } else { // Send to worker and receive result
+        int64_t input_meta[4] = {send_row_count, input.W, send_start_row, output_rows_for_this_proc};
+        MPI_Send(input_meta, 4, MPI_INT64_T, i, META_TAG, MPI_COMM_WORLD);
         MPI_Send(input.data[send_start_row], send_row_count * input.W, MPI_FLOAT, i, DATA_TAG, MPI_COMM_WORLD);
+
+        // Receive the trimmed result directly into the correct place in the final output matrix
+        float* receive_buffer = output.data[current_output_row];
+        MPI_Recv(receive_buffer, output_rows_for_this_proc * output_width, MPI_FLOAT, i, DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);       
       }
       current_output_row += output_rows_for_this_proc;
     }
 
-    current_output_row = 0; 
-    for (int i = 0; i < size; ++i) {
-      int64_t output_rows_for_this_proc = rows_per_proc + ((i < remainder) ? 1 : 0);
-      if (output_rows_for_this_proc == 0) {
-        continue;
-      }
-
-      if (i == 0) {
-        continue;
-      }
-
-      float* receive_buffer = output.data[current_output_row];
-      int64_t elements_to_receive = output_rows_for_this_proc * output_width;
-
-      MPI_Recv(receive_buffer,elements_to_receive,MPI_FLOAT,i,DATA_TAG,MPI_COMM_WORLD,MPI_STATUS_IGNORE);       
-
-      current_output_row += output_rows_for_this_proc;
-    }
-
-    if (f_path && H && W && g_path && kH && kW)
-    {
+    // --- (File writing and cleanup is unchanged) ---
+    if (f_path && H && W && g_path && kH && kW) {
       write_matrix(f_path, input);
       write_matrix(g_path, kernel);
     }
-  
-    if(o_path)
-      write_matrix(o_path, output);
-  
-    if(f_path)
-      free(f_path);
-    if(g_path)
-      free(g_path);
-    if(o_path)
-      free(o_path);
-    
+    if(o_path) write_matrix(o_path, output);
+    if(f_path) free(f_path);
+    if(g_path) free(g_path);
+    if(o_path) free(o_path);
     free_matrix(input);
     free_matrix(kernel);
     free_matrix(output);
-  } else { // worker
-    int64_t meta[4];
-    int64_t kernel_h, kernel_w, stride_h, stride_w;
-    MPI_Bcast(meta, 4, MPI_INT64_T, 0, MPI_COMM_WORLD);
-    kernel_h = meta[KERNEL_H];
-    kernel_w = meta[KERNEL_W];
-    stride_h = meta[STRIDE_H];
-    stride_w = meta[STRIDE_W];
-    Matrix kernel = create_matrix(kernel_h, kernel_w);
-    MPI_Bcast(kernel.data[0], kernel_h * kernel_w, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    int64_t input_meta[2];
-    int64_t input_h, input_w;
-    MPI_Recv(input_meta, 2, MPI_INT64_T, 0, META_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    input_h = input_meta[0];
-    input_w = input_meta[1];
-    Matrix local_input = create_matrix(input_h, input_w);
-    MPI_Recv(local_input.data[0], input_h * input_w, MPI_FLOAT, 0, DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-    Matrix local_output = conv2d(local_input, kernel, stride_h, stride_w);
-    MPI_Send(local_output.data, local_output.H * local_output.W, MPI_FLOAT, 0, DATA_TAG, MPI_COMM_WORLD);
-
-    free_matrix(local_input);
-    free_matrix(kernel);
-    free_matrix(local_output);
   }
 
   MPI_Finalize();
